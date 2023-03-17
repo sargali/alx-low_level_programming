bd2f7cdc8823e830a25d8a4c6999c579cca67d01
sargali <koki1191664@gmail.com>
2023-03-16 19:02:56 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - Entry point 
+ * main - A program that prints a line with puts function  
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	pust("\"ming is like building a multilingual puzzle");
+	printf("\"ming is like building a multilingual puzzle");
 	return (0);
 }
